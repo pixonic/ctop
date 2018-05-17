f05a55771b2750670b361d48068bc9fae1cc7084
Balaji Venkatesan <venkatesan.balaji@gmail.com>
2018-05-17 16:22:45 -0700

    added jmx username and password support

--- src/main/java/com/pixonic/ctop/Main.java
+++ src/main/java/com/pixonic/ctop/Main.java
@@ -8,26 +8,44 @@ import javax.management.ObjectName;
 import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
+import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         if (args.length == 0) {
-            System.out.println("Usage: java -jar ctop.jar <host:jmx_port> <key_space> [interval_sec(default: 10)]");
+            System.out.println("Usage: java -jar ctop.jar <host:jmx_port> <key_space> [interval_sec(default: 10)] [jmxUsername:jmxPassword]");
             return;
         }
 
         String hostAndPort = args[0];
         String keySpace = args[1];
+
         int interval = 10;
         if (args.length > 2) {
             interval = Integer.parseInt(args[2]);
         }
 
+        String jmxUsername = null;
+        String jmxPassword = null;
+        boolean hasCreds = false;
+        String[] creds = null;
+        if (args.length > 3) {
+            creds = args[3].trim().split(":", 2);
+            hasCreds = true;
+        }
+
+
         System.out.println("Connecting to " + hostAndPort + "...");
 
+
         final JMXServiceURL target = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://" + hostAndPort + "/jmxrmi");
-        final JMXConnector connector = JMXConnectorFactory.connect(target);
+
+
+        HashMap environment = new HashMap();
+        environment.put(JMXConnector.CREDENTIALS, creds);
+
+        final JMXConnector connector = JMXConnectorFactory.connect(target, (hasCreds) ? environment : null);
         final MBeanServerConnection remote = connector.getMBeanServerConnection();
 
         ObjectName storageMBean = new ObjectName("org.apache.cassandra.db:type=StorageService");
